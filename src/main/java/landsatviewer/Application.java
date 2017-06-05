bd31ef81de46f01872dc5daac770031a7bd8b947
David Bazile <dbazile@users.noreply.github.com>
2017-06-05 00:39:09 -0400

    Implement "get scene by ID"

--- src/main/java/landsatviewer/Application.java
+++ src/main/java/landsatviewer/Application.java
@@ -1,10 +1,15 @@
 package landsatviewer;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.mashape.unirest.http.ObjectMapper;
+import com.mashape.unirest.http.Unirest;
 import landsatviewer.planet.Client;
+import landsatviewer.planet.Scene;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
 
+import java.io.IOException;
 import java.time.Instant;
 import java.util.HashMap;
 import java.util.Map;
@@ -16,6 +21,30 @@ public class Application {
 
     public Application() {
         client = new Client(System.getenv("PLANET_API_KEY"));
+        initializeUnirest();
+    }
+
+    private void initializeUnirest() {
+        Unirest.setObjectMapper(new ObjectMapper() {
+            com.fasterxml.jackson.databind.ObjectMapper mapper = new com.fasterxml.jackson.databind.ObjectMapper();
+            public <T> T readValue(String s, Class<T> cls) {
+                try {
+                    return mapper.readValue(s, cls);
+                }
+                catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+            public String writeValue(Object o) {
+                try {
+                    return mapper.writeValueAsString(o);
+                }
+                catch (JsonProcessingException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
     }
 
     @GET
@@ -24,6 +53,13 @@ public class Application {
         HashMap<String, Double> status = new HashMap<>();
         status.put("uptime", (Instant.now().toEpochMilli() - startTimestamp) / 1000.0);
         return status;
+    }
+
+    @GET
+    @Path("/scene/{id}")
+    @Produces("application/json")
+    public Scene getScene(@PathParam("id") String id) throws Client.Error {
+        return client.getScene(id);
     }
 
     @GET
--- src/main/java/landsatviewer/planet/Client.java
+++ src/main/java/landsatviewer/planet/Client.java
@@ -5,10 +5,10 @@ import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
 
 import java.io.InputStream;
-import java.util.Scanner;
 
 public class Client {
-    public static final String TILE_URL = "https://tiles.planet.com/data/v1/Landsat8L1G/{sceneId}/{z}/{x}/{y}.png";
+    private static final String TILE_URL = "https://tiles.planet.com/data/v1/Landsat8L1G/{sceneId}/{z}/{x}/{y}.png";
+    private static final String SCENE_URL = "https://api.planet.com/data/v1/item-types/Landsat8L1G/items/{sceneId}";
     private final String apiKey;
 
     public Client(String apiKey) {
@@ -17,14 +17,35 @@ public class Client {
 
     public InputStream fetchTile(String sceneId, int x, int y, int z) throws Error {
         HttpResponse<InputStream> response;
+
         try {
             response = Unirest.get(TILE_URL)
                     .routeParam("x", String.valueOf(x))
                     .routeParam("y", String.valueOf(y))
                     .routeParam("z", String.valueOf(z))
                     .routeParam("sceneId", sceneId)
-                    .queryString("api_key", apiKey)
+                    .basicAuth(apiKey, "")
                     .asBinary();
+        }
+        catch (UnirestException e) {
+            throw new Error(e);
+        }
+
+        int status = response.getStatus();
+        if (status != 200) {
+            throw new Error(String.format("Planet returned HTTP %s", status));
+        }
+
+        return response.getBody();
+    }
+
+    public Scene getScene(String sceneId) throws Error {
+        HttpResponse<Scene> response;
+        try {
+            response = Unirest.get(SCENE_URL)
+                    .routeParam("sceneId", sceneId)
+                    .basicAuth(apiKey, "")
+                    .asObject(Scene.class);
         }
         catch (UnirestException e) {
             throw new Error(e);
